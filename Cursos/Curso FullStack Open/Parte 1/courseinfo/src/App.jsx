c90c05192eb2d6e5fee017b781070d30b2f692e9
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-12-21 12:50:53 -0500

    Update App.jsx

--- Cursos/Curso FullStack Open/Parte 1/courseinfo/src/App.jsx
+++ Cursos/Curso FullStack Open/Parte 1/courseinfo/src/App.jsx
@@ -1,67 +1,64 @@
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+const Header = (props) => (
+  <>
+    <h1>{props.name}</h1>
+  </>
+);
 
-const Part = (props) => {
-  return (
-    <>
-      <p>{props.name}: {props.exercises}</p>
-    </>
-  )
-}
+const Parts = (props) => (
+  <>
+    <br />
+    {props.parts.map(part => (
+      <p key={part.name}>{part.name} : {part.exercises}</p>
+    ))}
+  </>
+);
 
-const Content = (props) => {
-  return (
-    <>
-      <br />
-      <p><strong>Content:</strong></p>
-      <Part {...props.part1} />
-      <Part {...props.part2} />
-      <Part {...props.part3} />
-    </>
-  )
-}
+const Content = (props) => (
+  <>
+    <br />
+    <p><strong>Content:</strong></p>
+    <Parts parts={props.parts} />
+  </>
+);
 
-const Total = (props) => {
-  return (
-    <>
-      <br />
-      <p><strong>Number of exercises:</strong> {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
-  )
-}
+const Total = (props) => (
+  <>
+    <br />
+    <p><strong>Number of exercises:</strong> {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+  </>
+);
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <>
-      <Header course={course} />
-      
-      <Content part1={part1} part2={part2} part3={part3} />
-      
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
 
 

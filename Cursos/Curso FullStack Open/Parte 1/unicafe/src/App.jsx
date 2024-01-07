94142f432f5596a609bc9a4253ddab8a39ea82d6
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-01-06 19:00:27 -0500

    Update App.jsx

--- Cursos/Curso FullStack Open/Parte 1/unicafe/src/App.jsx
+++ Cursos/Curso FullStack Open/Parte 1/unicafe/src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 
 const Button = (props) => {
+
   return (
     <button onClick={props.handleClick}>
       {props.text}
@@ -21,16 +22,19 @@ const Votes = (props) => {
 }
 
 const Statistics = (props) => {
+
   return (
     <div>
       <h2>Statistics:</h2>
       <table>
-        <Votes text='Good' vote={props.voteG}/>
-        <Votes text='Neutral' vote={props.voteN}/> 
-        <Votes text='Bad' vote={props.voteB}/>
-        <Votes text='All' vote={props.voteG + props.voteN + props.voteB}/>
-        <Votes text='Average' vote={'...'}/>
-        <Votes text='Positives' vote={`${(props.voteG / (props.voteG + props.voteN + props.voteB)) * 100}%`}/>
+        <tbody>
+          <Votes text='Good' vote={props.voteG}/>
+          <Votes text='Neutral' vote={props.voteN}/>
+          <Votes text='Bad' vote={props.voteB}/>
+          <Votes text='All' vote={props.voteG + props.voteN + props.voteB}/>
+          <Votes text='Average' vote={props.avg / props.all}/>
+          <Votes text='Positives' vote={`${(props.voteG / (props.voteG + props.voteN + props.voteB)) * 100}%`}/>
+        </tbody>
       </table>
     </div>
   )
@@ -40,30 +44,32 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [prom, setProm] = useState(0)
+  const all = good + bad + neutral
 
   if (good  === 0 && neutral === 0 && bad === 0) {
     return (
       <>
         <h1>Give Feedback</h1>
-        <Button handleClick={() => {setGood(good + 1)}} text='Good'/>
-        <Button handleClick={() => {setNeutral(neutral + 1)}} text='Neutral'/>
-        <Button handleClick={() => {setBad(bad + 1)}} text='Bad'/>
+        <Button text='Good' handleClick={() => {setGood(good + 1); setProm(prom + 1)}}/>
+        <Button text='Neutral' handleClick={() => {setNeutral(neutral + 1); setProm(prom + 0)}}/>
+        <Button text='Bad' handleClick={() => {setBad(bad + 1); setProm(prom - 1)}} />
         <br />
         <h2>Statistics:</h2>
         <p>No feedback given</p>
       </>
     )
   }
-
+  
   else {
     return (
       <div>
         <h1>Give Feedback</h1>
-        <Button handleClick={() => {setGood(good + 1)}} text='Good'/>
-        <Button handleClick={() => {setNeutral(neutral + 1)}} text='Neutral'/>
-        <Button handleClick={() => {setBad(bad + 1)}} text='Bad'/>
-  
-        <Statistics voteG={good} voteN={neutral} voteB={bad}/>
+        <Button text='Good' handleClick={() => {setGood(good + 1); setProm(prom + 1)}}/>
+        <Button text='Neutral' handleClick={() => {setNeutral(neutral + 1); setProm(prom + 0)}}/>
+        <Button text='Bad' handleClick={() => {setBad(bad + 1); setProm(prom - 1)}} />
+        <br />
+        <Statistics voteG={good} voteN={neutral} voteB={bad} avg={prom} all={all}/>
       </div>
     )
   }

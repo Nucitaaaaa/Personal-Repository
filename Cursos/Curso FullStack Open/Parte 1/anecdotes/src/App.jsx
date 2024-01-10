5bf8e98729fb1f83c5e815e45298baeccce57e2b
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2024-01-10 17:47:32 -0500

    anecdotes

---
+++ Cursos/Curso FullStack Open/Parte 1/anecdotes/src/App.jsx
@@ -0,0 +1,63 @@
+
+import { useState } from 'react'
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+}
+
+const Anecdote = (props) => {
+  return <p>{props.anecdote[props.select]}</p>
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState([0,0,0,0,0,0,0,0])
+  console.log(vote)
+
+  const newAnec = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+  
+  const newVotes = () => {
+    const newVote = [...vote]
+    newVote[selected] += 1
+
+    setVote(newVote)
+  }
+
+  const maxVotes = () => {
+    const indMaxVote = vote.indexOf(Math.max(...vote))
+    return anecdotes[indMaxVote]
+  }
+
+  return (
+    <div>
+      <h1>Anecdotes</h1>
+      <Anecdote anecdote={anecdotes} select={selected}/>
+
+      <p>This anecdote has {vote[selected]} votes</p>
+      <Button text='Vote' handleClick={newVotes}/>
+
+      <Button text='Next' handleClick={() => {newAnec()}}/>
+
+    <h1>Anecdote with most votes</h1>
+    <p>{maxVotes()}</p>
+
+    </div>
+  )
+}
+
+export default App
